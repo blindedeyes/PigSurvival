0ea57aa483e2bc204be1e46b7dba72a2c67c40ad
Cody Ngo <codyngo13@gmail.com>
2022-12-04 00:32:50 -0600

    Setting up some menu stuff.

--- PigSurvival/Assets/Scripts/MenuScripts.cs
+++ PigSurvival/Assets/Scripts/MenuScripts.cs
@@ -2,11 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuScripts : MonoBehaviour
 {
+    public Slider volumeSlider;
+
+    public void Awake()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("VOLUME");
+    }
+
     public void LoadScene(string name)
     {
         SceneManager.LoadScene(name);
+    }
+
+    public void CloseGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void VolumeSliderUpdate()
+    {
+        PlayerPrefs.SetFloat("VOLUME", volumeSlider.value);
+        PlayerPrefs.Save();
     }
 }
